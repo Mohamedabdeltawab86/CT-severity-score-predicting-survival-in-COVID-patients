2836ab8d9429231add1f2ee54da2e3b30978ada0
Mohamed Abdeltawab <dr.m.tawab2@gmail.com>
2021-08-06 22:33:35 +0200

    First commit

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request
+import joblib
+
+app = Flask(__name__)
+model = joblib.load('model.h5')
+scaler = joblib.load('scaler.h5')
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    age = request.args.get('age')
+    gender = request.args.get("is_male", 1)
+    ggo = request.args.get('is_GGO', 1)
+    consolidation = request.args.get('is_consolidation', 1)
+    crazy_paving = request.args.get('is_crazy_paving', 1)
+    CT_severity = request.args.get("CT_severity")
+    survival = model.predict(scaler.transform[[age, gender, ggo, consolidation, crazy_paving, CT_severity]])
+    return render_template("index.html", survival = survival)
+
+
+
+if __name__ == "__main__":
+    app.run()
